b6624d27342459dd50ee2949651aab1190ad4d16
maggiezhao11 <maggiezhao06@hotmail.com>
2021-07-05 23:18:58 +0000

    add post route and install body parser

--- express_server.js
+++ express_server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+
+
 
 app.set("view engine", "ejs");//set ejs as the view engine
+app.use(bodyParser.urlencoded({extended: true})); //add middleware in order to review body of POST when its sent as a Buffer
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -33,10 +37,21 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+//adding a NEW route to show the form
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 //adding a route for /urls
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL,longURL: urlDatabase[req.params.shortURL]};
   // console.log("urlDatabase:", urlDatabase);
   // console.log("longURL:", urlDatabase[req.params.shortURL]);
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
